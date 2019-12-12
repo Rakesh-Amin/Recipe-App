7822218aa359af314208990782bb5470c6cfc25e
Rockey Martin <rockeymartin@builddirect.com>
2019-12-12 16:49:40 +0530

    Updates

---
+++ precache-manifest.6c0dd4926a0c796ec64f3a28173a6c24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3226aa77ba6842c09af68d9c4486953e",
+    "url": "/Rakesh-Amin/Recipe-App/index.html"
+  },
+  {
+    "revision": "1b939f4baa07f1746a6e",
+    "url": "/Rakesh-Amin/Recipe-App/static/css/main.73be0518.chunk.css"
+  },
+  {
+    "revision": "a24f9d4932b07654cc38",
+    "url": "/Rakesh-Amin/Recipe-App/static/js/2.1d09d5b1.chunk.js"
+  },
+  {
+    "revision": "1b939f4baa07f1746a6e",
+    "url": "/Rakesh-Amin/Recipe-App/static/js/main.9c137435.chunk.js"
+  },
+  {
+    "revision": "b97b08924ec70c4152c6",
+    "url": "/Rakesh-Amin/Recipe-App/static/js/runtime-main.b83d7178.js"
+  }
+]);
